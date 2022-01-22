bfd1b68c4faea23e6d061631257e61680c8f934f
Michael Tran <mhtran.hy@gmail.com>
2022-01-21 17:21:27 -0800

    Again button implementation

--- script.js
+++ script.js
@@ -2,17 +2,14 @@
 
 // Initializing the random number outside the click event handler b/c
 // if we put it inside the function, we would get a new number every click.
-const secretNumber = Math.trunc(Math.random()*20) + 1;
+let secretNumber = Math.trunc(Math.random()*20) + 1;
 let score = 20;
-
-// Logging to test guessing logic
-console.log(secretNumber);
 
 document.querySelector('.check').addEventListener('click', function() {
     // The user's input comes back as a String, so we need to convert to number
     const guess = Number(document.querySelector('.guess').value);
 
-    // Checking for value
+    // Checking for empty value
     if(!guess) {
         document.querySelector('.message').textContent = 'Please enter a number!';
 
@@ -20,6 +17,8 @@ document.querySelector('.check').addEventListener('click', function() {
     } else if (guess === secretNumber) { 
         document.querySelector('.message').textContent = 'Correct!';
         document.querySelector('.number').textContent = secretNumber;
+        document.querySelector('body').style.backgroundColor = '#60b347';
+        document.querySelector('.number').style.width = '30rem';
 
     // When player guesses too high
     } else if (guess > secretNumber) {  
@@ -41,4 +40,16 @@ document.querySelector('.check').addEventListener('click', function() {
             document.querySelector('.message').textContent = 'No more guesses available.';
         }
     }
+})
+
+// Again button - play again
+document.querySelector('.again').addEventListener('click', function() {
+    secretNumber = Math.trunc(Math.random()*20) + 1;
+    score = 20;
+    document.querySelector('.message').textContent = 'Start guessing...';
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.guess').value = '';
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width = '15rem';
 })
